3830272a54c1687f3a84104e7b9380f884d7ec9a
root <huaqing.xu@qq.com>
2021-08-01 08:26:07 +0000

    1

--- get_data/get_data.py
+++ get_data/get_data.py
@@ -1,9 +1,10 @@
+# http://baostock.com/
+
 import baostock as bs
 import pandas as pd
 
-#### 登陆系统 ####
 lg = bs.login()
-# 显示登陆返回信息
+
 print('login respond error_code:'+lg.error_code)
 print('login respond  error_msg:'+lg.error_msg)
 
